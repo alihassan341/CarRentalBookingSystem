f28d0755f9dcbd672966ec79ca62843a70da7ec1
AbdulBasit5096 <abdulbasit5096@gmail.com>
2022-12-31 18:24:49 +0500

    Created All Models + Few Changes in Signup form

--- CarRentalBooking/models.py
+++ CarRentalBooking/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime
 
 class client(models.Model):
     FirstName = models.CharField(max_length=255)
@@ -8,14 +9,29 @@ class client(models.Model):
     Email = models.CharField(max_length=25)
     Password = models.CharField(max_length=25)
     PetName = models.CharField(max_length=25)
-    Address = models.TextField()
 
+class contact(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.CharField(max_length=25)
+    subject = models.TextField(max_length=50)
+    message = models.CharField(max_length=255)
 
+class fleetmaster(models.Model):
+    fleetnumber = models.CharField(max_length=10)
+    fleetname = models.CharField(max_length=50)
+    fleetbrand = models.CharField(max_length=50)
+    enginecapacity = models.CharField(max_length=50)
+    colour = models.CharField(max_length=50)
+    fleetmodel = models.CharField(max_length=50)
+    fleetimg = models.CharField(max_length=50)
+    fleetcategory = models.CharField(max_length=50)
 
-# class contact(models.Model):
-#     name = models.CharField(max_length=122)
-#     email = models.CharField(max_length=122)
-#     phone = models.CharField(max_length=12)
-#     decs = models.TextField(max_length=122)
-#     Date = models.DateField(max_length=122)
-   
+class bookings(models.Model):
+    fleetmasterid = models.ForeignKey(fleetmaster, on_delete=models.CASCADE)
+    clientid = models.ForeignKey(client, on_delete=models.CASCADE)
+    bookingdate = models.DateTimeField(default=datetime.now,blank=True)
+    status = models.CharField(max_length=50)
+
+class testimonials(models.Model):
+    clientid = models.ForeignKey(client, on_delete=models.CASCADE)
+    remarks = models.CharField(max_length=255)
